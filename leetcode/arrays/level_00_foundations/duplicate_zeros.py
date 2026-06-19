fe666cff78a0d5603018d0e91be188a9d2f54e36
Debabrata Dey <debabratadey171295@gmail.com>
2026-06-19 12:50:35 +0530

    duplicate zeroes

--- leetcode/arrays/level_00_foundations/duplicate_zeros.py
+++ leetcode/arrays/level_00_foundations/duplicate_zeros.py
@@ -63,4 +63,8 @@ print(duplicate_Zeros(arr))
 
 #! Write a logic table by hand to solve the problem   
 #! Concept of virtual array  
-#! j points outside the real array                 
+#! j points outside the real array         
+
+#!       i 1  2   3   4   5   6   7   8       
+#!    arr=[1, 0,  2,  3,  0,  4,  5,  0]  _  _  _    m= 8   #! Application of auxillay concept of ghost array and ghost index
+#!       j 1  2   3   4   5   6   7   8   9  10 11 
